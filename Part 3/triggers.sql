f94794ac43b76e63735ba5f858ed117365ebb621
tobiasengblom <gusengblto@student.gu.se>
2021-02-18 09:37:05 +0100

    Finished function

--- Part 3/triggers.sql
+++ Part 3/triggers.sql
@@ -5,47 +5,40 @@ CREATE OR REPLACE VIEW CourseQueuePositions AS (
 -- SELECT course, student, place FROM CourseQueuePositions;
 
 CREATE OR REPLACE FUNCTION registerCourse () RETURNS TRIGGER AS $$
-DECLARE newStatus TEXT;
+DECLARE nrStudents INT;
 DECLARE	maxCapacity INT;
 DECLARE nrOfPrerequisites INT;
 DECLARE nrOfPassedPrerequisites INT;
 BEGIN
 	IF NOT EXISTS (SELECT idnr FROM Students WHERE idnr = NEW.student)
 	THEN RAISE EXCEPTION 'Student in input does not exist';
-	
 	ELSIF NOT EXISTS (SELECT code FROM Courses WHERE code = NEW.course)
 	THEN RAISE EXCEPTION 'Course in input does not exist';
 	END IF;
 	
 	IF EXISTS (SELECT course FROM PassedCourses WHERE PassedCourses.course = NEW.course AND PassedCourses.student = NEW.student)
 	THEN RAISE EXCEPTION 'Student already passed the course';
-	
 	ELSIF NEW.student IN (SELECT Registrations.student FROM Registrations
 					   WHERE Registrations.course = NEW.course)
 	THEN RAISE EXCEPTION 'Student already registered';
-	
 	ELSIF EXISTS (SELECT prerequisite FROM Prerequisite WHERE Prerequisite.course = NEW.course)
 	THEN 
 	nrOfPrerequisites := (SELECT COUNT (prerequisite) FROM Prerequisite WHERE Prerequisite.course = NEW.course);
 	nrOfPassedPrerequisites := (SELECT COUNT (student) FROM Prerequisite JOIN PassedCourses ON prerequisite = PassedCourses.course
-								WHERE student = NEW.student AND course = NEW.course);
+								WHERE student = NEW.student AND Prerequisite.course = NEW.course);
 		IF (nrOfPrerequisites > nrOfPassedPrerequisites)
 		THEN RAISE EXCEPTION 'Student have not passed all prerequisite courses';
+		ELSE 
+		maxCapacity := (SELECT capacity FROM LimitedCourses
+						WHERE NEW.course = course);
+		nrStudents := (SELECT COUNT (student) FROM Registered
+					   WHERE NEW.course = Registered.course);
+			IF (nrStudents >= maxCapacity)
+			THEN INSERT INTO WaitingList VALUES (NEW.student, NEW.course);
+			ELSE INSERT INTO Registered VALUES (NEW.student, NEW.course);
+			END IF;
 		END IF;
 	END IF;
-	newStatus := (SELECT status FROM Registrations
-			   WHERE NEW.student = student AND NEW.course = course);
-	maxCapacity := (SELECT capacity FROM LimitedCourses
-			   WHERE NEW.course = course);
-	/*
-	ELSE IF (SELECT prerequisite FROM Prerequisite WHERE NEW.course = Prerequisite.course) NOTNULL
-	THEN
-		FOREACH 
-	
-	ELSE
-		INSERT INTO Registrations 
-		VALUES (NEW.student, NEW.course, NEW.status);
-		*/
 	RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
