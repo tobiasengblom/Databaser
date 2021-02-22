3db24a33f51d9a4efe8dedf7ad2b52ddb56aa411
tobiasengblom <gusengblto@student.gu.se>
2021-02-22 09:30:02 +0100

    Added some more tests

--- Part 3/setup.sql
+++ Part 3/setup.sql
@@ -1,14 +1,3 @@
-\set QUIET true
-SET client_min_messages TO WARNING; 
-
-DROP SCHEMA public CASCADE;
-CREATE SCHEMA public;
-GRANT ALL ON SCHEMA public TO postgres;
-
--- Enable log messages again.
-SET client_min_messages TO NOTICE; 
-\set QUIET false
-
 -- Tables
 
 CREATE TABLE Programs (
