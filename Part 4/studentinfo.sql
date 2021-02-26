0e61d4f47c317bcd553cf281f58bc183880192c0
tobiasengblom <gusengblto@student.gu.se>
2021-02-26 15:16:10 +0100

    Created two arrays

--- Part 4/studentinfo.sql
+++ Part 4/studentinfo.sql
@@ -1,31 +1,36 @@
-/*
-CREATE OR REPLACE VIEW StudentInfo AS
-WITH 
-	BasicInfo AS 
-	(SELECT * FROM BasicInformation),
-	Finished AS
-	(SELECT Taken.student, Courses.name AS course, code, credits, grade
-	 FROM Courses, Taken
-	 WHERE code = Taken.course)
-SELECT BasicInfo.idnr AS student,
-	   name,
-	   login,
-	   program,
-	   branch,
-	   course,
-	   code,
-	   credits,
-	   grade FROM BasicInfo
-	   --COALESCE (status, NULL) AS status FROM BasicInfo
-LEFT OUTER JOIN Finished ON Finished.student = BasicInfo.idnr
-ORDER BY student;
-*/
+\c portal
+\set QUIT true
+SET client_min_messages TO WARNING;
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
+GRANT ALL ON SCHEMA public TO postgres;
+\set QUIET false
+\i setup.sql
+
+CREATE OR REPLACE VIEW FinishedCoursesWithNames AS (
+	SELECT student, Courses.name, course, grade, FinishedCourses.credits
+	FROM Courses, FinishedCourses
+	WHERE Courses.code = course
+);
+SELECT * FROM FinishedCoursesWithNames;
+
+CREATE OR REPLACE VIEW RegistrationsWithNames AS (
+	SELECT student, Courses.name, course, status
+	FROM Courses, Registrations
+	WHERE Courses.code = course
+);
+SELECT * FROM RegistrationsWithNames;
 
 SELECT jsonb_build_object(
-'student',idnr,
-'name',name, 
+'student', idnr,
+'name', BasicInformation.name, 
 'login', login, 
 'program', program, 
 'branch', branch,
-'finished', jsonb_agg (jsonb_build_object ('course', jsonb_build_object ('name', name) AS courseNames FROM Courses) AS finished FROM FinishedCourses) 
-) AS jsondata FROM BasicInformation WHERE idnr = '1111111111';
+'finished', jsonb_agg (jsonb_build_object ('course', FinishedCoursesWithNames.name, 'code', FinishedCoursesWithNames.course, 'credits', FinishedCoursesWithNames.credits, 'grade', FinishedCoursesWithNames.grade)),
+'registered', jsonb_agg (jsonb_build_object ('course', RegistrationsWithNames.name, 'code', RegistrationsWithNames.course, 'status', status))
+) AS jsondata FROM BasicInformation, FinishedCoursesWithNames, RegistrationsWithNames
+WHERE idnr = '2222222222' AND 
+FinishedCoursesWithNames.student = idnr AND
+RegistrationsWithNames.student = idnr
+GROUP BY BasicInformation.idnr, BasicInformation.name, BasicInformation.login, BasicInformation.program, BasicInformation.branch;
