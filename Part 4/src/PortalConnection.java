d04d8869571effe43b66ea43209e38eb6ca36306
tobiasengblom <gusengblto@student.gu.se>
2021-02-26 09:54:22 +0100

    Added login, program, branch to info

--- Part 4/src/PortalConnection.java
+++ Part 4/src/PortalConnection.java
@@ -66,7 +66,7 @@ public class PortalConnection {
 
         try (PreparedStatement st = conn.prepareStatement(
                 // replace this with something more useful
-                "SELECT jsonb_build_object('student',idnr,'name',name) AS jsondata FROM BasicInformation WHERE idnr=?"
+                "SELECT jsonb_build_object('student',idnr,'name',name, 'login', login, 'program', program, 'branch', branch) AS jsondata FROM BasicInformation WHERE idnr = ?"
         );) {
 
             st.setString(1, student);
